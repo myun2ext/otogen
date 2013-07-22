a8dd569b6dc677cf2e0cdfce1cc62c1c83785984
myun2 <myun2@nwhite.info>
2013-07-22 16:56:45 +0900

    init includes (otogen) !

---
+++ include/myun2/otogen.hpp
@@ -0,0 +1,6 @@
+#ifndef __github_com_myun2__otogen_HPP__
+#define __github_com_myun2__otogen_HPP__
+
+#include "myun2/otogen/otogen.hpp"
+
+#endif//__github_com_myun2__otogen_HPP__
---
+++ include/myun2/otogen/otogen.hpp
@@ -0,0 +1,4 @@
+#ifndef __github_com_myun2__otogen__otogen_HPP__
+#define __github_com_myun2__otogen__otogen_HPP__
+
+#endif//__github_com_myun2__otogen__otogen_HPP__
